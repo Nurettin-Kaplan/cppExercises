0f8b812b4ad02de5e3b75585a3c136314c67ab81
Nurettin-Kaplan <nurettinkaplaan@gmail.com>
2024-07-24 15:12:42 +0300

    Exercise 4 is done.

---
+++ 4_SquareAreaAndPerimeter.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+// a program that calculates the area and perimeter of a square given the length of one side
+int main(void){
+	
+	int side, area, perimeter;
+
+	cout << "Enter the length of one side of the square: ";
+	cin >> side;
+	
+	area = side * side;	
+	perimeter = side * 4;
+	
+	cout << "Area: " << area << endl;
+	cout << "Perimeter: " << perimeter << endl;
+	
+	return 0;
+}
