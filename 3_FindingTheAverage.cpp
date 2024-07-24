9ca33cf1f2213d4579a0aac64055a66e4aef7229
Nurettin-Kaplan <nurettinkaplaan@gmail.com>
2024-07-24 15:05:39 +0300

    Exercise 3 is done.

---
+++ 3_FindingTheAverage.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+// a program that asks the user to enter two numbers and prints the average of these numbers on the screen
+int main(void){
+	
+	int num1, num2;
+	float avg;
+
+	cout << "Enter the first number: ";
+	cin >> num1;
+	
+	cout << "Enter the second number: ";
+	cin >> num2;
+
+	avg = (num1 + num2) / 2;
+
+	cout << "Number: " << avg;
+	
+	return 0;
+}
