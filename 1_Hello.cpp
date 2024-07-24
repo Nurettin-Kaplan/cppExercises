aee2fa0b5683ee4fe469119d662e7c41a0f1c07d
Nurettin-Kaplan <nurettinkaplaan@gmail.com>
2024-07-24 14:57:25 +0300

    Exercises 1 and 2 completed.

---
+++ 1_Hello.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+// a program that prints the message 'Hello C++!' to the screen
+int main(void){
+
+    std::cout << "Hello C++!\n";
+
+    return 0;
+}
---
+++ 2_CoutAndCin.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+// a program that prompts the user to enter a number and then displays that number on the screen
+int main(void){
+	
+	int num;
+
+	cout << "Enter a number: ";
+	cin >> num;
+
+	cout << "Number: " << num;
+	
+	return 0;
+}
