b255f6d98b6717123b201924c0f99a08db405b13
Nurettin-Kaplan <nurettinkaplaan@gmail.com>
2024-07-25 14:29:46 +0300

    Exercise 9 is done.

---
+++ 9_AbsoluteValue.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int num;
+	
+	cout << "Enter the number you want to find the absolute value of: ";
+	cin >> num;
+	
+	cout << "Absoulte Value: " << abs(num) << "\n";
+	
+	return 0;
+}
